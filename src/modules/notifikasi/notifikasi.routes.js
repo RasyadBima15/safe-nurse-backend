1998cd32e2a0bf2274d1ee54915ea5bd8ee3063f
RasyadBima15 <rasyadbimasatya04@gmail.com>
2025-09-13 18:35:07 +0800

    fix routes notification

--- src/modules/notifikasi/notifikasi.routes.js
+++ src/modules/notifikasi/notifikasi.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getNotifikasi, getNewNotifikasi, createNotifikasi } from './notifikasi.controller.js';
+import { authenticateToken, authorizeRoles } from '../../middleware/auth.js';
 
 const router = Router();
 
 router.get('/', getNotifikasi);
 router.get('/new', getNewNotifikasi);
-router.post('/create', createNotifikasi);
+router.post('/create', authenticateToken, authorizeRoles('super_admin'), createNotifikasi);
 
 export default router;
