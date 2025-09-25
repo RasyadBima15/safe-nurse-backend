893fc9ae124689f2b768df8e11b76ba7159cf39d
RasyadBima15 <rasyadbimasatya04@gmail.com>
2025-09-25 23:47:43 +0800

    fix

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
 
