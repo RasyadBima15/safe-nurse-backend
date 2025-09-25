991eb1b61e19a56b476e0b39dfbb28060ec08679
RasyadBima15 <rasyadbimasatya04@gmail.com>
2025-09-25 20:24:29 +0800

    fix cors method

--- src/app.js
+++ src/app.js
@@ -21,9 +21,10 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
 app.options("*", cors());
