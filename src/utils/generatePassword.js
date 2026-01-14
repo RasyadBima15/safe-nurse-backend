c47977799da13882e4d6ff857ae0d629d41e38a3
RasyadBima15 <rasyadbimasatya04@gmail.com>
2026-01-14 12:46:24 +0800

    fix again

--- src/utils/generatePassword.js
+++ src/utils/generatePassword.js
@@ -1,12 +1,12 @@
 import crypto from 'crypto'
 
-export function generatePassword(length = 16) {
-  return crypto
-    .randomBytes(length)
-    .toString("base64") 
-    .replace(/[^a-zA-Z0-9]/g, '')
-    .slice(0, length);
-}
+// export function generatePassword(length = 16) {
+//   return crypto
+//     .randomBytes(length)
+//     .toString("base64") 
+//     .replace(/[^a-zA-Z0-9]/g, '')
+//     .slice(0, length);
+// }
 
 export function generatePassword() {
   return "12345";
